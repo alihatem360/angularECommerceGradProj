bc86a8c6fcad58b44b4524b35da9aa85ffe7362b
Omar Sayed <orhman@hotmail.com>
2022-08-15 01:09:45 +0200

    Add basic modules and basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
+import { ErrorModule } from './pages/error/error.module';
+import { HomeModule } from './pages/home/home.module';
+import { RegisterModule } from './pages/register/register.module';
+import { LoginModule } from './pages/login/login.module';
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', loadChildren: () => LoginModule },
+  { path: 'register', loadChildren: () => RegisterModule },
+  { path: 'home', loadChildren: () => HomeModule },
+  { path: 'error', loadChildren: () => ErrorModule },
+  { path: '**', redirectTo: 'error' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
