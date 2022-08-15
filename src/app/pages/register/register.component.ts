dcf9a6a3c66167cf316fa12ca1e128c87776f519
Omar Sayed <orhman@hotmail.com>
2022-08-16 00:38:20 +0200

    add dynamic forms, and email validators

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,15 +1,23 @@
+import { NgForm } from '@angular/forms';
+import { IUserRegister } from './../../models/user';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  userForm: IUserRegister = {
+    email: '',
+    password: '',
+    name: '',
+  };
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit(form: NgForm) {
+    //TODO handle register logic
   }
-
 }
